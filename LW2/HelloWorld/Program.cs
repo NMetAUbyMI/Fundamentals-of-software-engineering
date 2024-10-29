c6b1d57dd1083ba5731a5a33cdaeb6cafa33783f
NMetAUbyMI <nmetaubymi@gmail.com>
2024-10-29 09:20:53 +0200

    ver. 2.0

--- LW2/HelloWorld/Program.cs
+++ LW2/HelloWorld/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] agrs)
     {
-        Console.WriteLine("Hello, World!"); // Виведення тексту на екран
+        // Запитуємо у користувача перше число
+        Console.WriteLine("Введіть перше число: ");
+        string input1 = Console.ReadLine();
+
+        // Запитуємо у користувача друге число
+        Console.WriteLine("Введіть друге число: ");
+        string input2 = Console.ReadLine();
+
+        // Конвертація введених значень у тип double
+        double num1 = Convert.ToDouble(input1);
+        double num2 = Convert.ToDouble(input2);
+
+        // Виконання арифметичних операцій
+        Console.WriteLine($"Сума: {num1 + num2}");
+        Console.WriteLine($"Різниця: {num1 - num2}");
+        Console.WriteLine($"Добуток: {num1 * num2}");
+
+        // Обробка ділення на нуль
+        if (num2 != 0)
+        {
+            Console.WriteLine($"Частка: {num1 / num2}");
+        }
+        else
+        {
+            Console.WriteLine("Помилка: ділення на нуль неможливе!");
+        }
     }
 }
